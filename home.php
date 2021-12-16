630dc47218450866c0a09b9c815a16081f9ebecc
Ashok Chouhan <ashok.chouhan1708@gmail.com>
2021-12-16 17:07:12 +0530

    Update Date Function

--- home.php
+++ home.php
@@ -30,7 +30,14 @@ $res=$stm->fetchAll(PDO::FETCH_ASSOC);
 </head>
 <body>
     <div class="main">
-        <h1 style="text-align: center">Rajathan Technical University, Kota</h1>
+        <div class="header" style="display: flex">
+            <h1 style="width: 90%; text-align: center">Rajathan Technical University, Kota</h1>
+            <div class="form-group form-button">
+                <a href="index.php" type="submit" name="signin" id="signin" class="form-submit" value="Back">Back</a>
+            </div>
+        </div>
+
+
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,7 +45,7 @@ $res=$stm->fetchAll(PDO::FETCH_ASSOC);
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
-                    <li class="nav-item active">
+                    <li class="nav-item">
                         <a class="nav-link" href="#" onclick="showDetails()">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
